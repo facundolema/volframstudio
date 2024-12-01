b7a43de421fa421a2fb59ac7a7b9f97ccac330d1
facundolema <facurlema@gmail.com>
2024-12-01 01:47:34 -0300

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,25 @@
-<h1>Volfram Studio</h1>
+<div class="container">
+    <h1>Volfram Studio</h1>
+    <div style="display: flex; flex-direction: row; gap: 1rem;">
+        <div class="img-container"></div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ipsa excepturi nobis officia optio consequatur adipisci dolore provident vel ea ad, totam, perferendis sunt reprehenderit nemo? Corporis temporibus illo incidunt!</p>
+    </div>
+</div>
+
+<style>
+    h1 {
+        font-size: 4rem;
+    }
+    .img-container {
+      background-color: lightblue;
+      width: 600px;
+      height: 300px;
+      border: 1px solid black;
+    }
+    .container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 20px;
+    }
+</style>
