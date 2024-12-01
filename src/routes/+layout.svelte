47589fda3bffe4f52243c2a4aebee87d26dab216
facundolema <facurlema@gmail.com>
2024-12-01 14:01:13 -0300

    added google analytics

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-QSN0BQ79BY"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-QSN0BQ79BY');
+  </script>
+</svelte:head>
